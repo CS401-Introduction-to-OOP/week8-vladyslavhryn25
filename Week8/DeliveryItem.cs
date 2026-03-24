6f897cca96e3d2c38e3cff431e4f0233e9f60ef3
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-24 21:36:59 +0200

    Phase 1

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Week8;
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; }
+    public double Weight { get; }
+
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+    public abstract double CalculateCost();
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Трек-номер: {TrackingNumber}  Вага: {Weight} кг");
+    }
+}
