6256a775bf5e47f772073150cbeeaa058afea8c7
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-24 22:02:05 +0200

    Phase 2

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,18 @@
+﻿namespace Week8;
+
+public class CargoContainer <T> where T : DeliveryItem
+{
+    private List<T> _items = new List<T>();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+        Console.WriteLine($"Додано: {item.TrackingNumber}");
+    }
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (var item in _items)
+            total += item.CalculateCost();
+        return total;
+    }
+}
